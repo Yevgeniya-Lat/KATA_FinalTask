522ab218e429ed60553faca6eeb7f639a01bea11
Yevgeniya-Lat <e9618281262@gmail.com>
2022-05-06 23:21:35 +0300

    commit Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+
+Примеры:
+
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+
+["Russia", "Denmark", "Kazan"] -> [ ]
+
+*/
+
+
+string[] arrString = {"1234", "1567", "-2", "computer science"};
+
+for (int i = 0; i < arrString.Length; i++)
+{
+    string text = String.Empty;
+    text = arrString[i];
+
+    int length = text.Length;
+
+    string result = String.Empty;
+    for (int j = 0; j < length; j++)
+    {
+        if (length < 4)
+        {
+            result = result + $"{text[j]}";
+
+        }
+        else break;
+    }
+    System.Console.Write($"{result}" + " ");
+
+}
+
