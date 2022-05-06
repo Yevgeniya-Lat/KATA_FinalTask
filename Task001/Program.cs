4d09c4417c636796c8e2edc2789906f112cbdfa2
Yevgeniya-Lat <e9618281262@gmail.com>
2022-05-06 23:22:11 +0300

    commit Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] arrString = { "hello", "2", "world", ":-)" };
+
+for (int i = 0; i < arrString.Length; i++)
+{
+    string text = String.Empty;
+    text = arrString[i];
+
+    int length = text.Length;
+
+    string result = String.Empty;
+    for (int j = 0; j < length; j++)
+    {
+        if (length < 4)
+        {
+            result = result + $"{text[j]}";
+
+        }
+        else break;
+    }
+    System.Console.Write($"{result}" + " ");
+
+}
